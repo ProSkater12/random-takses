c90993a3a40b966581f63bbe5c33dcf746659ac8
Андрей Немытов <43293747+ProSkater12@users.noreply.github.com>
2020-07-04 23:58:27 +0500

    5 урок

---
+++ react/firstProj/react-intro/src/components/Article.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+
+class Article extends Component {
+    /* Обычная запись*/
+    /*constructor(props){
+        super(props)
+
+        this.state = {
+            isOpen: true
+        }
+
+    }*/
+
+    /*Эксперементальный синтаксис*/
+    state = {
+        isOpen: true
+    }    
+
+    render() {
+        const {article} = this.props
+        const body = this.state.isOpen && <section>{article.text}</section>
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+            </div>
+        )
+    }
+
+    /*Эксперементальный синтаксис*/
+    handleClick = () => {
+        alert('Жопа');
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+export default Article
